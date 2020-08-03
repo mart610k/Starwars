14476c8f049de97417496a7437ac3d7b34cb9087
martin (darkmtbg2) <martin-baage@hotmail.com>
2020-08-03 14:03:55 +0200

    Initial Data

---
+++ Starwars/MyNoRotationComparer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Starwars
+{
+    class MyNoRotationComparer : IEqualityComparer<Planet>
+    {
+        public bool Equals(Planet x, Planet y)
+        {
+            if (x.RotationPeriod > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int GetHashCode(Planet obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
